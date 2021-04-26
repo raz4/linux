8db1c98d126095f31212fd3c84bb83307121382e
Muslim Razi <razi.muslim@gmail.com>
2021-04-26 11:43:35 -0700

    use uint, print eax ebx ecx

--- test_cpuid/prog.c
+++ test_cpuid/prog.c
@@ -2,10 +2,14 @@
 
 int main()
 {
-	int a;
+	unsigned int a[3];
 	__asm__("mov $0x4FFFFFFF, %eax\n\t");
 	__asm__("cpuid\n\t");
-	__asm__("mov %%eax, %0\n\t":"=r" (a));
-	printf("Value in eax register is %d\n", a);
+	__asm__("mov %%eax, %0\n\t":"=r" (a[0]));
+	__asm__("mov %%ebx, %0\n\t":"=r" (a[1]));
+	__asm__("mov %%ecx, %0\n\t":"=r" (a[2]));
+	printf("Value in eax register is %u\n", a[0]);
+	printf("Value in ebx register is %u\n", a[1]);
+	printf("Value in ecx register is %u\n", a[2]);
 	return 0;
 }
