bcb3ca8a87b8c7fa8d597fdac93f5c4f8c2d9b13
Muslim Razi <razi.muslim@gmail.com>
2021-05-01 23:56:50 -0700

    update test_cpuid with 0x4FFFFFFE leaf

--- test_cpuid/prog.c
+++ test_cpuid/prog.c
@@ -3,6 +3,8 @@
 int main()
 {
 	unsigned int a[3];
+
+	printf("Testing 0x4FFFFFFF...\n");
 	__asm__("mov $0x4FFFFFFF, %eax\n\t");
 	__asm__("cpuid\n\t");
 	__asm__("mov %%eax, %0\n\t":"=r" (a[0]));
@@ -11,5 +13,15 @@ int main()
 	printf("Value in eax register is %u\n", a[0]);
 	printf("Value in ebx register is %u\n", a[1]);
 	printf("Value in ecx register is %u\n", a[2]);
+
+	printf("\nTesting 0x4FFFFFFE...\n\n");
+	for (unsigned int i = 0; i < 70; i++){
+		__asm__("mov $0x4FFFFFFE, %eax\n\t");
+		__asm__("mov %0, %%ecx\n\t":: "m"(i));
+		__asm__("cpuid\n\t");
+		__asm__("mov %%eax, %0\n\t":"=r" (a[0]));
+		printf("Exit Reason: %u, Count: %u\n", i, a[0]);
+	}
+
 	return 0;
 }
